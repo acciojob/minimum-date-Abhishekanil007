b5582dcca600975c9272b4413ac7555e420c1ef1
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-21 22:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function minDate(dates) {
   //write you code here
+	const dateObj = dates.map(dateString => new Date(dateString));
+
+	const minDate = new Date(Math.min.apply(null, dateObj));
+
+	const format = minDate.toISOString().slice(0, 10).replace(/-/g, '/');
+
+	return format;
 }
 
 // Do not change the code
@@ -14,7 +21,7 @@ var dates = [
   "2023/03/07",
   "2023/03/08",
   "2023/03/09",
-  "2023/03/10",
+  "2023/03/10",  
   "2023/03/11",
   "2023/03/12",
   "2023/03/13",
@@ -33,7 +40,7 @@ var dates = [
   "2023/03/26",
   "2023/03/27",
   "2023/03/28",
-  "2023/03/29",
+  "2023/03/29", 
   "2023/03/30",
 ];
 
